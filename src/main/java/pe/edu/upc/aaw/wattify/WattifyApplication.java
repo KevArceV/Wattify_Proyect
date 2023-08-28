a48b60f8cfcf544704ea066b448c562e052c7c23
Fabianick <fabianick2014@gmail.com>
2023-08-27 19:40:56 -0500

    prueba

---
+++ src/main/java/pe/edu/upc/aaw/wattify/WattifyApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.aaw.wattify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WattifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WattifyApplication.class, args);
+	}
+
+}

02c4e711f26f27e41bd7b344b808a2d30664b126
PieeroRS <95156536+PieeroRS@users.noreply.github.com>
2023-09-06 17:30:53 +0200

    Revert "entities"
    This reverts commit 07de501faac05efd2d22501e9f6730c713eb21de.

--- src/main/java/pe/edu/upc/aaw/wattify/repositories/IMetodoPagoRepositories.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.aaw.wattify.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.aaw.wattify.entities.MetodoPago;
-
-public interface IMetodoPagoRepositories extends JpaRepository<MetodoPago,Integer> {
-}

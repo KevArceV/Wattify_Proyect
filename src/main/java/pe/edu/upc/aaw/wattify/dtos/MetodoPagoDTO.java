4332393d207eb8c1474a013e22971f5a3e240bde
Fabianick <fabianick2014@gmail.com>
2023-09-19 16:22:30 -0500

    Cambiar:Metodo de pago

--- src/main/java/pe/edu/upc/aaw/wattify/dtos/MetodoPagoDTO.java
+++ src/main/java/pe/edu/upc/aaw/wattify/dtos/MetodoPagoDTO.java
@@ -1,14 +1,13 @@
 package pe.edu.upc.aaw.wattify.dtos;
 
 import pe.edu.upc.aaw.wattify.entities.Usuario;
+
+import javax.persistence.Column;
 import java.time.LocalDate;
 public class MetodoPagoDTO {
     private int id;
-    private int Tipo;
-    private int Credencial_de_tarjeta;
-    private LocalDate Fecha_de_vencimiento;
-    private String Copia_de_Seguridad;
-    private Usuario usuario;
+    private String Tipo_de_pago;
+    private String Titular;
 
     public int getId() {
         return id;
@@ -18,43 +17,19 @@ public class MetodoPagoDTO {
         this.id = id;
     }
 
-    public int getTipo() {
-        return Tipo;
+    public String getTipo_de_pago() {
+        return Tipo_de_pago;
     }
 
-    public void setTipo(int tipo) {
-        Tipo = tipo;
+    public void setTipo_de_pago(String tipo_de_pago) {
+        Tipo_de_pago = tipo_de_pago;
     }
 
-    public int getCredencial_de_tarjeta() {
-        return Credencial_de_tarjeta;
+    public String getTitular() {
+        return Titular;
     }
 
-    public void setCredencial_de_tarjeta(int credencial_de_tarjeta) {
-        Credencial_de_tarjeta = credencial_de_tarjeta;
-    }
-
-    public LocalDate getFecha_de_vencimiento() {
-        return Fecha_de_vencimiento;
-    }
-
-    public void setFecha_de_vencimiento(LocalDate fecha_de_vencimiento) {
-        Fecha_de_vencimiento = fecha_de_vencimiento;
-    }
-
-    public String getCopia_de_Seguridad() {
-        return Copia_de_Seguridad;
-    }
-
-    public void setCopia_de_Seguridad(String copia_de_Seguridad) {
-        Copia_de_Seguridad = copia_de_Seguridad;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
+    public void setTitular(String titular) {
+        Titular = titular;
     }
 }
